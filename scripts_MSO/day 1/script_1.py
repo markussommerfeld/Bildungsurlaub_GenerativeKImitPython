df18c0cc05e59badd6cd87dcfe3d130777fa9fce
MSommerfeld <markus_sommerfeld@yahoo.de>
2025-11-24 14:58:40 +0100

    Chat Groq API
    Frage was ist der Sinn des Lebens.
    Irgend ein Problem mit environments?

--- scripts_MSO/day 1/script_1.py
+++ scripts_MSO/day 1/script_1.py
@@ -1,6 +1,18 @@
 #%% load environment variables  
 from dotenv import load_dotenv, find_dotenv
+from langchain_groq import ChatGroq
 import os
 load_dotenv(find_dotenv())
 
-os.getenv("GROQ_API_KEY")
+# Test if key available
+#os.getenv("GROQ_API_KEY")
+#os.getenv("OPEN_ROUTER_API_KEY")
+# %% create Groq model
+model = ChatGroq(model="openai/gpt-oss-120b") #finds automatically this API:, api_key=os.getenv("GROQ_API_KEY")
+# %% Anfrage ans model
+user_query = "Was ist der Sinn des Lebens?"
+response = model.invoke(user_query)
+
+# %% Ausgabe ansehen
+print(response.content)
+# response.model_dump()
