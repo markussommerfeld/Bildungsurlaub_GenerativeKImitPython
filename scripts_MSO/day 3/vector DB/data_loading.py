7dc74f11e4c7f1242a56977314face8d6c0ac11a
MSommerfeld <markus_sommerfeld@yahoo.de>
2025-11-26 10:22:35 +0100

    wrote code to read multiple files and types
    moved documents to check,
    added faust
    text recognition in images (OCR), gescannte Texte als Bilder gespeichert, dann lade Tesseract, Poppler, pdf2image, pytesseract

--- scripts_MSO/day 3/vector DB/data_loading.py
+++ scripts_MSO/day 3/vector DB/data_loading.py
@@ -37,3 +37,39 @@ docs_dir = loader_dir.load()
 print(f'Number of documents in directory: {len(docs_dir)}')
 print(f'First document in directory content: {docs_dir[1].page_content[5300:5500]}...')  # print first 500 characters
 print(f'First document in directory metadata: {docs_dir[1].metadata}')
+
+#%% load different file types
+from langchain_pymupdf4llm import PyMuPDF4LLMLoader
+
+def select_loader(file_path):
+    _, file_extension = os.path.splitext(file_path)
+    # format to lower case to ensure case insensitivity
+    file_extension = file_extension.lower()
+
+    if file_extension == '.txt':
+        #pass: this is just a placeholder when coding an if statement without any action
+        return TextLoader(file_path, encoding='utf8')
+    elif file_extension == '.pdf':
+        return PyMuPDF4LLMLoader(file_path, mode='page')
+        # mode literal: single, page (siehe MIRO board)
+        # mode single: entire document as one page
+        # mode page: each page as separate document)
+    else:
+        #raise ValueError(f"Unsupported file type: {file_extension}")
+        return None
+    
+    
+loader_all = DirectoryLoader(current_dir, glob="**/*",exclude = "*.py", loader_cls=select_loader,
+                show_progress=True, 
+                use_multithreading=True)
+docs_all = loader_all.load()
+print(f'Number of documents in directory with different file types: {len(docs_all)}')
+
+# THIS IS FOR THE SPECIAL CASE THAT THE PDF CONTAINS IMAGES WITH TEXT
+# If you do not know, first try detect as txt and if that is empty, try OCR
+
+#text recognition in images (OCR) can be added with additional libraries such as pytesseract or easyocr
+#however, this is not implemented in langchain yet as of June 2024
+# install Poppler for Windows, Add to PATH or set POPPLER_PATH
+# Install Tesseract OCR and add optionally set: TESSERACT_CMD
+# Python deps: pip install: pdf2image, pytesseract
